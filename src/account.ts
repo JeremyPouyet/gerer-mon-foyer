cd88de82be5ad56b14ec84bc23dec3fc2449ded7
Jeremy Pouyet <jeremy.pouyet@gmail.com>
2024-09-15 03:04:30 +0200

    jsdoc

--- src/account.ts
+++ src/account.ts
@@ -2,18 +2,37 @@ import { emptyTransactions, newId, valueAs } from './helpers'
 import type { ID, Transaction, TransactionFunctional, TransactionList, TransactionRecord } from "./types"
 import { TransactionType } from "./types"
 
+/**
+ * Interface representing account related settings.
+ * @interface AccountSettings
+ */
 interface AccountSettings {
+  /**
+   * An object indicating whether to show expenses and incomes.
+   *
+   * @type {Object}
+   * @property {boolean} [TransactionType.Expense] Whether to show expenses.
+   * @property {boolean} [TransactionType.Income] Whether to show incomes.
+   */
   show: {
     [TransactionType.Expense]: boolean,
     [TransactionType.Income]: boolean
   }
 }
 
+
+/**
+ * Constructs the default account settings based on provided partial settings.
+ *
+ * @param {Partial<AccountSettings>} [settings={}] Partial settings to merge with the default ones.
+ * @returns {AccountSettings} The complete account settings.
+ */
 function buildSettings(settings: Partial<AccountSettings> = {}) : AccountSettings {
   return {
     show: Object.assign({[TransactionType.Expense]: true, [TransactionType.Income]: true}, settings.show)
   }
 }
+
 
 export default class Account {
   readonly expenses: TransactionRecord
@@ -34,10 +53,21 @@ export default class Account {
     this.updateSum(TransactionType.Expense)
   }
 
+  /**
+   * Updates the sum of transactions for a specified transaction type.
+   *
+   * @param {TransactionType} transactionType The type of transactions to update (either Income or Expense).
+   */
   updateSum(transactionType: TransactionType) {
     this[transactionType].sum = Object.values(this[transactionType].values).reduce((sum, transaction) => sum + valueAs(transaction), 0)
   }
 
+  /**
+   * Adds a new transaction to the specified transaction type.
+   *
+   * @param {TransactionType} transactionType The type of transaction to add (either Income or Expense).
+   * @param {TransactionFunctional} transaction The transaction details to add.
+   */
   transactionAdd(transactionType: TransactionType, transaction: TransactionFunctional) : void {
     const { name, frequency, value } = transaction;
     const trimmedName = name.trim()
@@ -51,6 +81,12 @@ export default class Account {
     this.onTransactionChange?.()
   }
 
+  /**
+   * Deletes a transaction from the specified transaction type.
+   *
+   * @param {TransactionType} transactionType The type of transaction to delete (either Income or Expense).
+   * @param {Transaction} transaction The transaction to delete.
+   */
   transactionDelete(transactionType: TransactionType, transaction: Transaction) : void {
     // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
     delete this[transactionType].values[transaction.id]
@@ -58,6 +94,13 @@ export default class Account {
     this.onTransactionChange?.()
   }
 
+  /**
+   * Updates an existing transaction with new details.
+   *
+   * @param {TransactionType} transactionType The type of transaction to update (either Income or Expense).
+   * @param {ID} id The ID of the transaction to update.
+   * @param {Partial<Transaction>} updates The updates to apply to the transaction.
+   */
   transactionUpdate(transactionType: TransactionType, id: ID, updates: Partial<Transaction>) {
     const transaction = this[transactionType].values[id]
 
@@ -72,6 +115,12 @@ export default class Account {
     }
   }
 
+  /**
+   * Retrieves a sorted list of transactions for a specified transaction type.
+   *
+   * @param {TransactionType} transactionType The type of transactions to retrieve (either Income or Expense).
+   * @returns {TransactionList} A list of transactions sorted by value in descending order.
+   */
   transactionSorted(transactionType: TransactionType) : TransactionList {
     const transactionRecord = this[transactionType]
 
