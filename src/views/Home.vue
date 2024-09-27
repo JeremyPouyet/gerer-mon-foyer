af071e63421ffcab8ac1b1135b153fe2ab55ef6b
Jeremy Pouyet <jeremy.pouyet@gmail.com>
2024-09-27 19:42:13 +0200

    Introducing Camille and Sam

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,7 +14,8 @@
         L’importance d’une répartition équitable
       </h2>
       <p class="text-start">
-        Les revenus des membres d’un foyer sont souvent inégaux, en raison de facteurs tels que des différences salariales entre professions, une interruption de carrière, ou des inégalités structurelles comme l’écart de salaires entre les hommes et les femmes. Pourtant, malgré ces écarts, la répartition des dépenses communes se fait souvent de manière égale, à 50/50, ce qui désavantage la personne gagnant le moins, en lui imposant une charge financière disproportionnée.
+        Les revenus des membres d’un foyer sont souvent inégaux, en raison de facteurs tels que des différences salariales entre professions, d’interruptions de carrière, ou d’inégalités structurelles comme l’écart de salaires entre hommes et femmes.
+        Malgré ces écarts, les dépenses communes sont souvent réparties de manière égale (50/50), désavantageant ainsi une seconde fois la personne gagnant le moins, en lui imposant une charge financière disproportionnée.
       </p>
     </section>
 
@@ -23,16 +24,17 @@
         Éxemple simple de répartition
       </h2>
       <p class="text-start">
-        Prenons un couple dans lequel un des membres gagne 2400€ par mois et l’autre 1600€. Le premier représente 60% des revenus du foyer et l’autre 40%.
-        Cependant, la personne qui touche 1600€ rembourse en plus un prêt étudiant de 200€ par mois! Voici comment seront réparties 500€ de dépenses communes :
+        Prenons un foyer dans lequel Camille gagne 2400€ par mois, et Sam 1600€, tout en remboursant un prêt étudiant de 200€.
+        Voici comment seront réparties 500€ de dépenses communes:
       </p>
       <ul class="text-start step-list">
-        <li>Le revenu mensuel disponible de la personne avec un prêt est de 1600€ - 200€ = 1400€.</li>
+        <li>Camille représente 60% des revenus du foyer et Sam 40%.</li>
+        <li>Cependant, le revenu mensuel disponible de Sam est en réalité de 1600€ - 200€ = 1400€.</li>
         <li>Le revenu total disponible du foyer est donc 2400€ + 1400€ = 3800€.</li>
-        <li>La personne qui gagne 2400€ paiera alors 63% des dépenses communes (315€).</li>
-        <li>La personne qui gagne 1600€ ne paiera que 37% (185€).</li>
-        <li>Avec une répartition 50/50, la personne qui gagne 1600€ aurait payé 250€, soit une économie de 7 800€ sur 10 ans grâce à cette méthode !</li>
+        <li>Camille paiera alors 63% des dépenses communes (315€).</li>
+        <li>Sam ne paiera que 37% (185€).</li>
       </ul>
+      <p>Avec une répartition 50/50, Sam aurait payé 250€, ce qui représente un avantage de 7 800€ sur 10 ans avec cette méthode.</p>
     </section>
 
     <section class="mb-5 text-center text-block">
@@ -43,13 +45,13 @@
         Pour chaque membre du foyer, il suffit de renseigner :
       </p>
       <ul class="text-start step-list">
-        <li>Les revenus (salaires, allocations, etc.).</li>
-        <li>Les dépenses contraintes personnelles (crédits, essence, assurances, etc.).</li>
-        <li>Les dépenses communes du foyer (loyer, factures, etc.).</li>
+        <li>Les revenus (salaires, allocations, etc…).</li>
+        <li>Les dépenses contraintes personnelles (crédits, essence, assurances, etc…).</li>
+        <li>Les dépenses communes du foyer (loyer, eau, gaz, etc…).</li>
       </ul>
       <p class="text-start">
-        L’outil soustrait automatiquement les dépenses contraintes des revenus individuels pour une répartition juste.
-        Il vous attribue ensuite un pourcentage des dépenses communes à payer, basé sur vos revenus disponibles.
+        L’outil soustrait automatiquement les dépenses contraintes des revenus individuels pour une répartition juste,
+        vous attribuant ensuite un pourcentage des dépenses communes à payer, basé sur vos revenus disponibles.
         Utilisez ensuite le
         <!-- eslint-disable-next-line vue/singleline-html-element-content-newline -->
         <RouterLink to="/simulator">simulateur de dépenses ponctuelles</RouterLink> pour diviser équitablement le prix d’un achat unique, et
