067c16cbef4c24f5129fc3201956fc9d8dcffb31
Jeremy Pouyet <jeremy.pouyet@gmail.com>
2024-11-20 23:57:53 +0100

    test payment table

--- src/components/simulator/AdvancedSimulator.vue
+++ src/components/simulator/AdvancedSimulator.vue
@@ -219,10 +219,78 @@ onMounted(() => {
         </button>
       </div>
       <div v-else>
-        <span class="form-label fw-bold d-block">Payments</span>
+        <span class="form-label fw-bold d-block mt-4">Payments</span>
         <div class="table-responsive shadowed-border mb-3">
           <table class="table table-hover mb-0">
+            <tr>
+              <th colspan="3">
+                Hélène - Total: 450
+              </th>
+            </tr>
+            <tr>
+              <th>
+                Date
+              </th>
+              <th>
+                Valeur
+              </th>
+              <th>
+                Commentaire
+              </th>
+            </tr>
+            <tr>
+              <td>
+                12/12/2024
+              </td>
+              <td>
+                250
+              </td>
+              <td>
+                Leroy Merlin
+              </td>
+            </tr>
+            <tr>
+              <th colspan="3">
+                Jérémy - Total: 650
+              </th>
+            </tr>
+            <tr>
+              <th>
+                Date
+              </th>
+              <th>
+                Valeur
+              </th>
+              <th>
+                Commentaire
+              </th>
+            </tr>
+            <tr>
+              <td>
+                12/12/2024
+              </td>
+              <td>
+                250
+              </td>
+              <td>
+                Leroy Merlin
+              </td>
+            </tr>
+            <tr>
+              <td>
+                12/12/2024
+              </td>
+              <td>
+                250
+              </td>
+              <td>
+                Leroy Merlin
+              </td>
+            </tr>
           </table>
+        </div>
+        <div v-if="projectState === ProjectStates.Frozen">
+          Ajouter une dépense
         </div>
       </div>
     </div>
