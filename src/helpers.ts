76685d0a4b490e313918fb108558761695fa0bda
Jeremy Pouyet <jeremy.pouyet@gmail.com>
2025-02-03 23:16:55 +0100

    h?

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,7 +2,7 @@ import { create, divideDependencies, evaluateDependencies, subtractDependencies 
 
 import { Frequency, TransactionType } from './types'
 import type { ID, Transaction } from './types'
-import { type Ref, computed, h } from 'vue'
+import { type Ref, computed } from 'vue'
 import type Account from './account'
 
 import { Modal } from 'bootstrap'
