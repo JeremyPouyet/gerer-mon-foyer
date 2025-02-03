78e67c5298dbd75de22fea725bfa45b8fc8fb448
Jeremy Pouyet <jeremy.pouyet@gmail.com>
2025-02-03 23:15:29 +0100

    use confirm modal

---
+++ src/components/structure/ConfirmModal.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div id="confirmModal" class="modal fade" tabindex="-1">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 id="confirmModalLabel" class="modal-title">
+            Confirmation
+          </h5>
+          <button aria-label="Close" class="btn-close" data-bs-dismiss="modal" type="button" />
+        </div>
+        <div class="modal-body">
+          Êtes-vous sûr de vouloir continuer ?
+        </div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" data-bs-dismiss="modal" type="button">
+            Annuler
+          </button>
+          <button id="confirmBtn" class="btn btn-primary" type="button">
+            Confirmer
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
