d934028a9f669a0f7fa295372768dd8c45b167c0
Jeremy Pouyet <jeremy.pouyet@gmail.com>
2025-02-08 15:52:07 +0100

    some aria attributes

--- src/components/structure/ConfirmModal.vue
+++ src/components/structure/ConfirmModal.vue
@@ -10,16 +10,27 @@ defineProps<{ message: string, callback: (() => void) | undefined }>()
           <h5 id="confirmModalLabel" class="modal-title">
             Confirmation
           </h5>
-          <button aria-label="Close" class="btn-close" data-bs-dismiss="modal" type="button" />
+          <button aria-label="Fermer la fenêtre" class="btn-close" data-bs-dismiss="modal" type="button" />
         </div>
         <div class="modal-body">
           {{ message }}
         </div>
         <div class="modal-footer">
-          <button class="btn btn-secondary" data-bs-dismiss="modal" type="button">
+          <button
+            aria-label="Annuler l’action"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+            type="button"
+          >
             Annuler
           </button>
-          <button id="confirmBtn" class="btn btn-primary" type="button" @click="callback?.()">
+          <button
+            id="confirmBtn"
+            aria-label="Confirmer l’action"
+            class="btn btn-primary"
+            type="button"
+            @click="callback?.()"
+          >
             Confirmer
           </button>
         </div>
